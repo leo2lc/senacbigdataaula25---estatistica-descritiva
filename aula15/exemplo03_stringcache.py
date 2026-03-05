7ce6dc5c5c6d51e04e494cbd1625c869c3d1b9f8
leo2lc <leonardolucas-c@hotmail.com>
2026-03-05 20:44:19 -0300

    string cash e scan parquet

---
+++ aula15/exemplo03_stringcache.py
@@ -0,0 +1,35 @@
+import polars as pl
+from datetime import datetime
+
+ENDERECO_DADOS = './../DADOS/PARQUET/NovoBolsaFamilia/'
+
+try:
+    print('Iniciando o processamento Lazy()')
+    inicio = datetime.now()
+
+    with pl.StringCache():
+
+        # Método Lazy "scan_parquet" cria um plano de execução, não carregando os TODOS os dados diretamente
+        # diretamente na memória, porém o plano é implementado
+        lazy_plan = (
+            pl.scan_parquet(ENDERECO_DADOS + 'bolsa_familia.parquet')
+            .select(['NOME MUNICÍPIO', 'VALOR PARCELA'])
+            .with_columns([
+                pl.col('NOME MUNICÍPIO').cast(pl.Categorical)
+            ])
+            .group_by('NOME MUNICÍPIO')             # agrupa pelo município
+            .agg(pl.col('VALOR PARCELA').sum())     # soma o valor da parcela
+            .sort('VALOR PARCELA', descending=True)
+        )
+
+    # print(lazy_plan)
+    # collect() executa o plano de execução, ele traz de fato os dados que estão estabelecidos no plano
+    df_bolsa_familia = lazy_plan.collect()
+    print(df_bolsa_familia.head(10))
+
+    fim = datetime.now()
+    print(f'Tempo de execução: {fim - inicio}')
+    print('Leitura do Arquivo parquet realizada com sucesso')
+
+except Exception as e:
+    print(f'Erro ao obter dados {e}')
